e45f2392f3e06b8cbafce9a074972eb993bc06e7
yelizondo <yubeg971@gmail.com>
2018-04-25 13:32:45 -0600

    Added __tostring function for printing

--- vector.lua
+++ vector.lua
@@ -17,15 +17,20 @@ c = function(arg)
 		end,
 
 		__add = function(table,o)
+			local new = c{}
+			for i = 1, #_table do 
+				new[i]= _table[i] + o 
+			end
+			return new
 		end,
 
 		__sub = function(table,o)
 		end,
 
-		__mul = function(table,o)
+		__mul = function(table, o)
 			local new = c{}
 			for i = 1, #_table do 
-				new[i]= _table[i]*o 
+				new[i]= _table[i] * o 
 			end
 			return new
 		end,
@@ -49,16 +54,30 @@ c = function(arg)
 		end,
 
 		__tostring = function(table)
+			local result = ""
+
+			for i = 1,#_table do
+				result = result  .. _table[i] .. "\t"
+			end
+
+			return result
+		end,
+
+		__len = function(table)
+			return #_table
 		end
 
 	}
 	setmetatable(table, metatable)
 	return table
 end
-
+--[[
   local x = c{10,20,30,40}
   local y = x * 2
   print(y[1],y[2],y[3])
   local z = y * 10
 print(z[1],z[2],z[3])
+]]
 
+local x = c{1,2,3}
+print(x)
